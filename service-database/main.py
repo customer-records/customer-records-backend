52b10a10d2c81ea859ba78b948815e24f2897d67
maratkasyanov1 <onouhjioj@gmail.com>
2025-04-15 06:01:03 +0300

    added db info

--- service-database/main.py
+++ service-database/main.py
@@ -47,36 +47,27 @@ def initialize_db():
     try:
         # Проверяем, есть ли уже данные в базе
         if not session.query(User).first():
-            # Добавляем категории услуг для стоматологии с массивами услуг (3 категории)
+            # Добавляем категории услуг с массивами услуг
             categories = [
                 CategoryService(
-                    name_category="Ортодонт",
-                    time_width_minutes_end=60,
+                    name_category="Хирург имплантолог",
+                    time_width_minutes_end=10,
                     services_array=[
-                        "Консультация",
-                        "Установка брекет-систем",
-                        "Коррекция прикуса у детей",
-                        "Ретенционный период"
+                        "Консультация хирурга имплантация/удаление",
                     ]
                 ),
                 CategoryService(
-                    name_category="Терапевтическая стоматология",
-                    time_width_minutes_end=45,
+                    name_category="Терапевт-ортопед",
+                    time_width_minutes_end=15,
                     services_array=[
-                        "Лечение кариеса",
-                        "Лечение пульпита",
-                        "Реставрация зубов",
-                        "Лечение периодонтита"
+                        "Первичная консультация терапевта​",
                     ]
                 ),
                 CategoryService(
-                    name_category="Хирургия",
-                    time_width_minutes_end=90,
+                    name_category="Стоматолог-терапевт",
+                    time_width_minutes_end=15,
                     services_array=[
-                        "Удаление зуба",
-                        "Имплантация",
-                        "Синус-лифтинг",
-                        "Костная пластика"
+                        "Первичная консультация терапевта​",
                     ]
                 )
             ]
@@ -85,15 +76,15 @@ def initialize_db():
 
             # Добавляем стоматологическую клинику
             company = CompanyDescription(
-                company_name="Denta - rell",
+                company_name="Denta Rell",
                 company_description="Современная стоматологическая клиника с высококвалифицированными специалистами",
                 company_adress_country="Россия",
-                company_adress_city="Москва",
-                company_adress_street="Стоматологическая",
-                company_adress_house_number="15",
-                company_adress_house_number_index="123456",
-                time_work_start=datetime.strptime("08:00", "%H:%M").time(),
-                time_work_end=datetime.strptime("20:00", "%H:%M").time(),
+                company_adress_city="Казань",
+                company_adress_street="Проспект Победы",
+                company_adress_house_number="35 Б",
+                company_adress_house_number_index="420000",
+                time_work_start=datetime.strptime("09:00", "%H:%M").time(),
+                time_work_end=datetime.strptime("19:00", "%H:%M").time(),
                 weekdays_work_1=True,
                 weekdays_work_2=True,
                 weekdays_work_3=True,
@@ -107,55 +98,67 @@ def initialize_db():
 
             # Хэшируем пароль для владельца
             salt = bcrypt.gensalt()
-            hashed_password = bcrypt.hashpw("dentist123".encode('utf-8'), salt)
+            hashed_password = bcrypt.hashpw("owner123".encode('utf-8'), salt)
 
-            # Добавляем владельца (главного врача)
+            # Добавляем владельца (Гадисов Ренат Фамильевич)
             owner = User(
                 role="owner",
-                email="chief@denta-rell.ru",
+                email="renat@dentapro.ru",
                 password=hashed_password.decode('utf-8'),
-                name="Алексей",
-                last_name="Петров",
-                sur_name="Сергеевич",
-                phone_number="+79123456789",
-                id_category_service=categories[0].id,
-                google_api_key="dent_api_key",
-                google_client_id="dent_client_id",
-                google_calendar_id="dent_calendar_id",
-                chat_id="123456789",
-                tg_name="chief_dentist"
+                name="Ринат",
+                last_name="Сергеев",
+                sur_name="Леонидович",
+                phone_number="79172759797",
+                id_category_service=categories[1].id,  # Хирург имплантолог
+                google_api_key="dentapro_api_key",
+                google_client_id="dentapro_client_id",
+                google_calendar_id="dentapro_calendar_id",
+                chat_id="100000001",
+                tg_name="dr_gadisov"
             )
             session.add(owner)
             session.commit()
 
-            # Добавляем стоматологов (2 специалиста)
-            dentists = [
+            # Добавляем работников
+            workers = [
                 User(
                     role="worker",
-                    email="dentist1@denta-rell.ru",
+                    email="rinat@dentapro.ru",
                     password=bcrypt.hashpw("doctor1pass".encode('utf-8'), salt).decode('utf-8'),
-                    name="Ирина",
-                    last_name="Иванова",
-                    sur_name="Владимировна",
-                    phone_number="+79876543210",
-                    id_category_service=categories[1].id,
-                    chat_id="987654321",
-                    tg_name="dr_ivanova"
+                    name="Ренат",
+                    last_name="Гадисов",
+                    sur_name="Фамильевич",
+                    phone_number="79274770444",
+                    id_category_service=categories[0].id,  # Терапевт-ортопед
+                    chat_id="100000002",
+                    tg_name="dr_sergeev"
                 ),
                 User(
                     role="worker",
-                    email="dentist2@denta-rell.ru",
+                    email="svetlana@dentapro.ru",
                     password=bcrypt.hashpw("doctor2pass".encode('utf-8'), salt).decode('utf-8'),
-                    name="Сергей",
-                    last_name="Смирнов",
-                    sur_name="Александрович",
-                    phone_number="+79876543211",
-                    id_category_service=categories[2].id,
-                    chat_id="555555555",
-                    tg_name="dr_smirnov"
+                    name="Светлана",
+                    last_name="Бареева",
+                    sur_name="Геннадьевна",
+                    phone_number="79872954242",
+                    id_category_service=categories[2].id,  # Стоматолог-терапевт
+                    chat_id="100000003",
+                    tg_name="dr_bareeva"
+                ),
+                User(
+                    role="worker",
+                    email="alfiya@dentapro.ru",
+                    password=bcrypt.hashpw("doctor3pass".encode('utf-8'), salt).decode('utf-8'),
+                    name="Альфия",
+                    last_name="Шарипова",
+                    sur_name="Маратовна",
+                    phone_number="79969029242",
+                    id_category_service=categories[2].id,  # Стоматолог-терапевт
+                    chat_id="100000004",
+                    tg_name="dr_sharipova"
                 )
             ]
-            session.add_all(dentists)
+            session.add_all(workers)
             session.commit()
 
             # Добавляем клиентов
@@ -163,147 +166,165 @@ def initialize_db():
                 Client(
                     email="client1@example.com",
                     password=bcrypt.hashpw("clientpass1".encode('utf-8'), salt).decode('utf-8'),
-                    name="Мария",
-                    last_name="Сидорова",
-                    phone_number="+79111111111",
-                    tg_name="mary_sidorova",
-                    chat_id="111111111"
+                    name="Иван",
+                    last_name="Иванов",
+                    phone_number="79111111111",
+                    tg_name="ivan_ivanov",
+                    chat_id="200000001"
                 ),
                 Client(
                     email="client2@example.com",
                     password=bcrypt.hashpw("clientpass2".encode('utf-8'), salt).decode('utf-8'),
-                    name="Андрей",
-                    last_name="Кузнецов",
-                    phone_number="+79222222222",
-                    tg_name="andrew_kuznetsov",
-                    chat_id="222222222"
+                    name="Елена",
+                    last_name="Петрова",
+                    phone_number="79222222222",
+                    tg_name="elena_petrova",
+                    chat_id="200000002"
                 )
             ]
             session.add_all(clients)
             session.commit()
 
-            # Добавляем временные слоты на 09.04.2025
-            date_09 = datetime(2025, 4, 9).date()
-            time_slots_09 = [
-                # Слоты для владельца (owner)
+            # Добавляем временные слоты на 10.04.2025
+            date_10 = datetime(2025, 4, 15).date()
+            time_slots_10 = [
+                # Слоты для владельца (Гадисов Ренат)
                 TimeSlot(
                     id_category_service=categories[0].id,
                     id_employer=owner.id,
-                    date=date_09,
+                    date=date_10,
                     time_start=datetime.strptime("09:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[0].id
                 ),
                 TimeSlot(
                     id_category_service=categories[0].id,
                     id_employer=owner.id,
-                    date=date_09,
+                    date=date_10,
                     time_start=datetime.strptime("11:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[0].id
                 ),
-                TimeSlot(
-                    id_category_service=categories[0].id,
-                    id_employer=owner.id,
-                    date=date_09,
-                    time_start=datetime.strptime("14:00", "%H:%M").time(),
-                    id_time_width_minutes_end=categories[0].id
-                ),
                 
-                # Слоты для первого стоматолога
+                # Слоты для Сергеева Рината (Терапевт-ортопед)
                 TimeSlot(
                     id_category_service=categories[1].id,
-                    id_employer=dentists[0].id,
-                    date=date_09,
-                    time_start=datetime.strptime("10:30", "%H:%M").time(),
+                    id_employer=workers[0].id,
+                    date=date_10,
+                    time_start=datetime.strptime("10:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[1].id
                 ),
                 TimeSlot(
                     id_category_service=categories[1].id,
-                    id_employer=dentists[0].id,
-                    date=date_09,
+                    id_employer=workers[0].id,
+                    date=date_10,
                     time_start=datetime.strptime("13:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[1].id
                 ),
                 
-                # Слоты для второго стоматолога
+                # Слоты для Бареевой Светланы (Стоматолог-терапевт)
                 TimeSlot(
                     id_category_service=categories[2].id,
-                    id_employer=dentists[1].id,
-                    date=date_09,
-                    time_start=datetime.strptime("11:00", "%H:%M").time(),
+                    id_employer=workers[1].id,
+                    date=date_10,
+                    time_start=datetime.strptime("09:30", "%H:%M").time(),
                     id_time_width_minutes_end=categories[2].id
                 ),
                 TimeSlot(
                     id_category_service=categories[2].id,
-                    id_employer=dentists[1].id,
-                    date=date_09,
+                    id_employer=workers[1].id,
+                    date=date_10,
+                    time_start=datetime.strptime("14:00", "%H:%M").time(),
+                    id_time_width_minutes_end=categories[2].id
+                ),
+                
+                # Слоты для Шариповой Альфии (Стоматолог-терапевт)
+                TimeSlot(
+                    id_category_service=categories[2].id,
+                    id_employer=workers[2].id,
+                    date=date_10,
+                    time_start=datetime.strptime("10:30", "%H:%M").time(),
+                    id_time_width_minutes_end=categories[2].id
+                ),
+                TimeSlot(
+                    id_category_service=categories[2].id,
+                    id_employer=workers[2].id,
+                    date=date_10,
                     time_start=datetime.strptime("15:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[2].id
                 )
             ]
 
-            # Добавляем временные слоты на 10.04.2025
-            date_10 = datetime(2025, 4, 10).date()
-            time_slots_10 = [
-                # Слоты для владельца (owner)
+            # Добавляем временные слоты на 11.04.2025
+            date_11 = datetime(2025, 4, 16).date()
+            time_slots_11 = [
+                # Слоты для владельца (Гадисов Ренат)
                 TimeSlot(
                     id_category_service=categories[0].id,
                     id_employer=owner.id,
-                    date=date_10,
+                    date=date_11,
                     time_start=datetime.strptime("10:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[0].id
                 ),
                 TimeSlot(
                     id_category_service=categories[0].id,
                     id_employer=owner.id,
-                    date=date_10,
+                    date=date_11,
                     time_start=datetime.strptime("12:30", "%H:%M").time(),
                     id_time_width_minutes_end=categories[0].id
                 ),
-                TimeSlot(
-                    id_category_service=categories[0].id,
-                    id_employer=owner.id,
-                    date=date_10,
-                    time_start=datetime.strptime("16:00", "%H:%M").time(),
-                    id_time_width_minutes_end=categories[0].id
-                ),
                 
-                # Слоты для первого стоматолога
+                # Слоты для Сергеева Рината (Терапевт-ортопед)
                 TimeSlot(
                     id_category_service=categories[1].id,
-                    id_employer=dentists[0].id,
-                    date=date_10,
-                    time_start=datetime.strptime("09:30", "%H:%M").time(),
+                    id_employer=workers[0].id,
+                    date=date_11,
+                    time_start=datetime.strptime("09:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[1].id
                 ),
                 TimeSlot(
                     id_category_service=categories[1].id,
-                    id_employer=dentists[0].id,
-                    date=date_10,
-                    time_start=datetime.strptime("14:00", "%H:%M").time(),
+                    id_employer=workers[0].id,
+                    date=date_11,
+                    time_start=datetime.strptime("14:30", "%H:%M").time(),
                     id_time_width_minutes_end=categories[1].id
                 ),
                 
-                # Слоты для второго стоматолога
+                # Слоты для Бареевой Светланы (Стоматолог-терапевт)
                 TimeSlot(
                     id_category_service=categories[2].id,
-                    id_employer=dentists[1].id,
-                    date=date_10,
-                    time_start=datetime.strptime("11:30", "%H:%M").time(),
+                    id_employer=workers[1].id,
+                    date=date_11,
+                    time_start=datetime.strptime("11:00", "%H:%M").time(),
                     id_time_width_minutes_end=categories[2].id
                 ),
                 TimeSlot(
                     id_category_service=categories[2].id,
-                    id_employer=dentists[1].id,
-                    date=date_10,
-                    time_start=datetime.strptime("17:00", "%H:%M").time(),
+                    id_employer=workers[1].id,
+                    date=date_11,
+                    time_start=datetime.strptime("16:00", "%H:%M").time(),
+                    id_time_width_minutes_end=categories[2].id
+                ),
+                
+                # Слоты для Шариповой Альфии (Стоматолог-терапевт)
+                TimeSlot(
+                    id_category_service=categories[2].id,
+                    id_employer=workers[2].id,
+                    date=date_11,
+                    time_start=datetime.strptime("10:30", "%H:%M").time(),
+                    id_time_width_minutes_end=categories[2].id
+                ),
+                TimeSlot(
+                    id_category_service=categories[2].id,
+                    id_employer=workers[2].id,
+                    date=date_11,
+                    time_start=datetime.strptime("15:30", "%H:%M").time(),
                     id_time_width_minutes_end=categories[2].id
                 )
             ]
 
-            session.add_all(time_slots_09 + time_slots_10)
+            session.add_all(time_slots_10 + time_slots_11)
             session.commit()
 
-            logger.info("Тестовые данные для стоматологии успешно добавлены в базу данных")
+            logger.info("Данные для стоматологической клиники успешно добавлены в базу данных")
     except Exception as e:
         logger.error(f"Ошибка при инициализации базы данных: {e}")
         session.rollback()
